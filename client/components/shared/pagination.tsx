6ffc4b3becd3e793e947de346a79874e4497ad5a
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-04-08 01:20:44 +0900

    Add browse page UI

---
+++ client/components/shared/pagination.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+function Pagination() {
+	return (
+		<div className='flex w-full items-center justify-center gap-2 mt-4'>
+			<Button size={'sm'}>Prev</Button>
+			<p>1</p>
+			<Button size={'sm'}>Next</Button>
+		</div>
+	)
+}
+
+export default Pagination
