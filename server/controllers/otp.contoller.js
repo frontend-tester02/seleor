34d7848a87e54df34332bda3d092ffa36a3b88cf
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-04-26 01:42:18 +0900

    Add OTP email

--- server/controllers/otp.contoller.js
+++ server/controllers/otp.contoller.js
@@ -1,3 +1,26 @@
-class OtpController {}
+const mailService = require('../services/mail.service')
+
+class OtpController {
+	async sendOtp(req, res, next) {
+		try {
+			const { email } = req.body
+			await mailService.sendOtpEmail(email)
+			res.json({ message: 'OTP send successfully' })
+		} catch (error) {
+			next(error)
+		}
+	}
+
+	async verifyOtp(req, res, next) {
+		try {
+			const { email, otp } = req.body
+			const result = await mailService.verifyOtp(email, otp)
+
+			res.json(result)
+		} catch (error) {
+			next(error)
+		}
+	}
+}
 
 module.exports = new OtpController()
--- server/routes/otp.js
+++ server/routes/otp.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
+const otpController = require('../controllers/otp.contoller')
 
-router.post('/send-otp', (req, res) => {})
-router.post('/verify-otp', (req, res) => {})
-router.post('/resend-otp', (req, res) => {})
+router.post('/send', otpController.sendOtp)
+router.post('/verify', otpController.verifyOtp)
 
 module.exports = router
