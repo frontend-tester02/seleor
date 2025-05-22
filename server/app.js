f735f56a979d9d972d21799af6fa6adc2bfc3992
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-05-23 02:00:47 +0900

    Change server port

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(errorMiddleware)
 
 const bootstrap = async () => {
 	try {
-		const PORT = process.env.PORT || 5000
+		const PORT = process.env.PORT || 8080
 		mongoose
 			.connect(process.env.MONGO_URI)
 			.then(() => console.log('Connected to MongoDB'))
